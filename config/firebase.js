b2f09242f71ddf27414586b17a1257c87e623588
Akash Roy <109346212+teamdaggers9@users.noreply.github.com>
2022-07-15 16:22:52 +0530

    Firebase setup

---
+++ config/firebase.js
@@ -0,0 +1,15 @@
+const dev = {
+  apiKey: "AIzaSyBa2Zotg9QiaoKvTTEsTsytb3W42EOBKUA",
+  authDomain: "data-dojo.firebaseapp.com",
+  projectId: "data-dojo",
+  storageBucket: "data-dojo.appspot.com",
+  messagingSenderId: "389218574637",
+  appId: "1:389218574637:web:069e41068b583fa2cb33a4",
+  measurementId: "G-GEZS34HMCN",
+};
+
+const firebaseConfig = dev;
+
+export default {
+  ...firebaseConfig,
+};
