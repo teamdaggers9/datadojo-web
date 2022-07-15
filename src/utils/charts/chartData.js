cd5d7afb52ba77e7d9403f852d014855a19dafe8
max61077 <max61077@gmail.com>
2022-07-15 18:51:28 +0530

    Pie chart and Column chart added

---
+++ src/utils/charts/chartData.js
@@ -0,0 +1,15 @@
+export const columnChartData = [
+    ["John", 10000],
+    ["Jake", 12000],
+    ["Peter", 13000],
+    ["James", 10000],
+    ["Mary", 9000]
+];
+
+export const pieChartData = [
+    { x: "Employee A", value: 637166 },
+    { x: "Employee B", value: 721630 },
+    { x: "Employee C", value: 148662 },
+    { x: "Employee D", value: 78662 },
+    { x: "Employee E", value: 90000 }
+];
