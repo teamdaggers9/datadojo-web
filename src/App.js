449033645782acd756cad918a650abcc7eb940ca
max61077 <max61077@gmail.com>
2022-07-17 12:08:17 +0530

    React version update

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
   };
 
   useEffect(() => {
-    console.log({ dataSet: MOCK_DATA.default });
+    // console.log({ dataSet: MOCK_DATA.default });
     // createNewCollection('projects', MOCK_DATA.default);
     fetchMasterData();
   }, []);
